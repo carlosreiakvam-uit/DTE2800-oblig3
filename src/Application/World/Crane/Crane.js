898354e59821c399f71b9f60da0a2545628f4617
carlos reiakvam <cre032@uit.no>
2022-10-29 08:45:16 +0200

    updated model

--- src/Application/World/Crane/Crane.js
+++ src/Application/World/Crane/Crane.js
@@ -1,6 +1,5 @@
 import VehicleBody from "./VehicleBody";
 import * as THREE from 'three'
-import CraneHouse from "./CraneHouse";
 import Application from '../../Application.js'
 import {position} from "three/examples/jsm/nodes/shadernode/ShaderNodeBaseElements";
 import {Vector3} from "three";
@@ -8,15 +7,12 @@ import {Vector3} from "three";
 export default class Crane {
     constructor() {
         const application = new Application()
-        const items = application.resources.items
-
         const craneGroup = new THREE.Group()
 
         const vehicleBody = new VehicleBody()
 
-
-
-        application.scene.add(vehicleBody)
+        craneGroup.add(vehicleBody)
+        application.scene.add(craneGroup)
     }
 
 
