3ae1bdbdc44b7527658c09171c80733294467f57
carlos reiakvam <cre032@uit.no>
2022-11-01 15:41:22 +0100

    forenklet kodestruktur

--- src/Application/World/Crane/Booms/BoomAssembled.js
+++ src/Application/World/Crane/Booms/BoomAssembled.js
@@ -4,25 +4,8 @@ import BoomBase from "./BoomBase/BoomBase.js";
 export default class BoomAssembled {
     constructor() {
         this.group = new THREE.Group()
-        this.addBoomBase()
-        this.group.position.set(0.3, 2, -0.05)
-    }
-
-    addBoomBase() {
-        let xAngle = 8
-        let zAngle = 8
-        let length = 2
-
-        this.boomBase = new BoomBase(length, xAngle, zAngle).group;
-        this.group.add(this.boomBase);
-    }
-
-
-    setPosition(x, y, z) {
-        this.group.position.set(x, y, z)
-    }
-
-    setScale(scale) {
-        this.group.scale.multiplyScalar(scale)
+        this.group.position.set(0.3, 2, -0.05) // initial position
+        this.group.add(new BoomBase(2, 8, 8));
     }
 }
+
