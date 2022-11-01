9e6de4a12e622e2315189d3a3c0bade818ea1c2e
carlos reiakvam <cre032@uit.no>
2022-11-01 15:41:47 +0100

    forenklet kodestruktur

--- src/Application/World/Crane/Booms/BoomAssembled.js
+++ src/Application/World/Crane/Booms/BoomAssembled.js
@@ -5,7 +5,7 @@ export default class BoomAssembled {
     constructor() {
         this.group = new THREE.Group()
         this.group.position.set(0.3, 2, -0.05) // initial position
-        this.group.add(new BoomBase(2, 8, 8));
+        this.group.add(new BoomBase(2, 8, 8).group);
     }
 }
 
